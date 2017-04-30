ca2ac762fc5e67ebcf8900f31a39f05359b06bcb
Rob Smart <rob.s.smart@gmail.com>
2017-04-30 16:17:05 +0100

    add -y flag to rm command to accept cmd line prompt

--- index.js
+++ index.js
@@ -18,27 +18,27 @@ var q = require("q");
 var os = require("os");
 
 module.exports = {
-    
-    
+
+
     ls: function(callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
         exec('docker-machine ls', function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
 
             //NAME      ACTIVE   DRIVER       STATE     URL                         SWARM   DOCKER    ERRORS
-            //default   -        virtualbox   Running   tcp://192.168.99.100:2376           v1.10.3   
+            //default   -        virtualbox   Running   tcp://192.168.99.100:2376           v1.10.3
 
-           
+
             var lines = stdout.split(os.EOL);
-            
+
 
 
             //getting the start and end positions for the subsequent substr operation
@@ -60,18 +60,18 @@ module.exports = {
 
             // clean up the output
             for(var i = 0; i < lines.length ; i++) {
-                
+
                 var stringarray = [];
 
                 for(var j = 0;j<letterboundaries.length-1;j++) {
                     var newString = lines[i].substring(letterboundaries[j],letterboundaries[j+1]);
                     stringarray.push(newString);
-                } 
+                }
 
                 linearrays.push(stringarray);
             }
-            
-            
+
+
             var json = [];
             var headers = [];
 
@@ -95,364 +95,364 @@ module.exports = {
 
                 json.push(machineObj);
             }
-            
+
             d.resolve(json);
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     },
-    
+
     create: function(machineName,options,callback) {
-        
+
         var d = q.defer();
-        
+
         var flags="";
-        
+
         for (var key in options) {
             if (options.hasOwnProperty(key)) {
                 flags += " --"+key+" "+ options[key];
             }
         }
-        
+
         var command = 'docker-machine create '+flags+' '+machineName;
-        
+
         console.log(command);
-        
+
         var exec = require('child_process').exec;
         exec(command, function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             module.exports.inspect(machineName).then(function(output){
-                
+
                 d.resolve(output);
-                
-            }).fail(function(err){ 
-            
+
+            }).fail(function(err){
+
                 d.reject(err);
             });
-            
-            
-        }); 
-        
+
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
-        
+
     },
-    
-    
-    
+
+
+
     rm: function(machineName,callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
-        exec('docker-machine rm '+machineName, function (error, stdout, stderr) {
-            
+        exec('docker-machine rm -y '+machineName, function (error, stdout, stderr) {
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             d.resolve(stdout);
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     },
-    
-    
+
+
     active: function(callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
         exec('docker-machine active', function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             d.resolve(stdout);
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     },
-    
-    
+
+
     inspect: function(machineName,callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
         exec('docker-machine inspect '+machineName, function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-            
+
             try {
                 var machine = JSON.parse(stdout);
-                
+
             } catch (e){
                 d.reject(e);
             }
-                
+
             d.resolve(machine);
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     },
-    
-    
-    
+
+
+
     regenerateCerts: function(machineName,callback) {
-        
+
         var d = q.defer();
-        
+
         var command = 'docker-machine regenerate-certs -f '+machineName;
-        
+
         console.log(command);
-        
+
         var exec = require('child_process').exec;
         exec(command, function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             module.exports.inspect(machineName).then(function(output){
-                
+
                 d.resolve(output);
-                
-            }).fail(function(err){ 
-            
+
+            }).fail(function(err){
+
                 d.reject(err);
             });
-            
-            
-        }); 
-        
+
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
-        
+
     },
-    
-    
-    
-    
+
+
+
+
     kill: function(machineName,callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
         exec('docker-machine kill '+machineName, function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             d.resolve(stdout);
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     },
-    
-    
+
+
     start: function(machineName,callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
         exec('docker-machine start '+machineName, function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             d.resolve(stdout);
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     },
-    
-    
-    
+
+
+
     stop: function(machineName,callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
         exec('docker-machine stop '+machineName, function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             d.resolve(stdout);
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     },
-    
-    
-    
-    
+
+
+
+
     restart: function(machineName,callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
         exec('docker-machine restart '+machineName, function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             d.resolve(stdout);
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     },
-    
-    
-    
+
+
+
     upgrade: function(machineName,callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
         exec('docker-machine upgrade '+machineName, function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             d.resolve(stdout);
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     },
-    
-    
+
+
     ssh: function(machineName, command, callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
         exec('docker-machine ssh '+machineName+ ' '+command, function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             d.resolve(stdout);
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     },
-    
-    
-    
+
+
+
     url: function(machineName,callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
         exec('docker-machine url '+machineName, function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             d.resolve(stdout);
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     },
-    
-    
-    
+
+
+
     ip: function(machineName,callback) {
-        
+
         var d = q.defer();
-      
+
         var exec = require('child_process').exec;
         exec('docker-machine ip '+machineName, function (error, stdout, stderr) {
-            
+
             if(error) {
                 d.reject(stderr);
                 return;
             }
-                
+
             d.resolve(stdout.replace('\n', ''));
-            
-        }); 
-        
+
+        });
+
         // allow standard node callbacks
         d.promise.nodeify(callback);
-        
+
         return d.promise;
     }
-    
+
 };
